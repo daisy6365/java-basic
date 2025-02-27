39bd62e596a41b1e407cfefe4ae52d766d2ea71b
han dabin <gksekqls9808@inu.ac.kr>
2025-02-27 15:43:51 +0900

    feat: final field and constant (2)

--- src/final1/Constant.java
+++ src/final1/Constant.java
@@ -1,6 +1,13 @@
 package final1;
 
-//상수
+/**
+ * 상수
+ * public 이기 때문에 어디에서나 접근 가능
+ * 전반적으로 변수를 사용해야하는 설정값들이 있기 때문에,
+ * 이러한 상수들은 public으로 두고 사용한다.
+ *
+ * @ Runtime에 값을 변경할 수 없다. 상수를 변경하려면 프로그램을 종료하고, 코드를 변경해야 함
+ */
 public class Constant {
     //수학 상수
     public static final double PI = 3.14;
