3c65ed7ec6d35e957724cbaa36a25d8c607f74a6
han dabin <gksekqls9808@inu.ac.kr>
2025-02-27 15:30:51 +0900

    feat: final field and constant (1)

--- src/final1/ConstructInit.java
+++ src/final1/ConstructInit.java
@@ -1,9 +1,11 @@
 package final1;
 
 public class ConstructInit {
-
+    // 클래스의 final 변수
+    // 초기값을 선언하면 생성자를 통해서도 할당 불가
     final int value;
 
+    // 생성자를 통해 값을 할당. -> 이후에는 할당 불가
     public ConstructInit(int value) {
         this.value = value;
     }
--- src/final1/FinalLocalMain.java
+++ src/final1/FinalLocalMain.java
@@ -1,12 +1,18 @@
 package final1;
 
 public class FinalLocalMain {
-
+    /**
+     * final 로 선언된 변수는 최초 한번만 값할당이 가능
+     * 이후 값을 변경하면 compile error 발생
+     *
+     * 오류내용
+     * - Variable 'data1' might already have been assigned to
+     */
     public static void main(String[] args) {
         //final 지역 변수1
         final int data1;
         data1 = 10; //최초 한번만 할당 가능
-        //data1 = 20; //컴파일 오류
+//        data1 = 20; //컴파일 오류
 
         //final 지역 변수2
         final int data2 = 10;
@@ -14,6 +20,12 @@ public class FinalLocalMain {
         method(10);
     }
 
+    /**
+     *
+     * 메서드 호출 시, final로 된 parameter 값을 할당한다면
+     * 이후 값변경이 불가능함
+     * @param parameter
+     */
     static void method(final int parameter) {
         //parameter = 20; //컴파일 오류
     }
