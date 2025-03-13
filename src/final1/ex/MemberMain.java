2c3e4473b7926b5410aa0ca38db6e44a6877cb14
daisy6365 <gksekqls9808@naver.com>
2025-03-13 22:55:13 +0900

    feat: final finish

--- src/final1/ex/MemberMain.java
+++ src/final1/ex/MemberMain.java
@@ -5,6 +5,8 @@ public class MemberMain {
     public static void main(String[] args) {
         Member member = new Member("myId", "kim");
         member.print();
+        // changeData를 통해 값을 변경하려고 해도,
+        // final값이니까 변경되지 않은 채 "myId"로 출력 됨
         member.changeData("myId2", "seo");
         member.print();
     }
