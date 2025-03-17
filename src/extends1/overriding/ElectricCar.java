f6a90876192103a78ed10b92e5da14bae662c4b6
daisy6365 <gksekqls9808@inu.ac.kr>
2025-03-17 17:19:09 +0900

    feat: 상속관계와 @Override

--- src/extends1/overriding/ElectricCar.java
+++ src/extends1/overriding/ElectricCar.java
@@ -2,8 +2,15 @@ package extends1.overriding;
 
 public class ElectricCar extends Car {
 
+    /**
+     * @Override를 통해 부모로부터 상속된 기능을 재정의 함을 표시함
+     * 이동한다 -> 빠르게 이동한다.
+     *
+     * 해당 어노테이션을 통해 컴파일 시 오버라이드를 확인함
+     */
     @Override
     public void move() {
+        // 이미 자식 인스턴스에 move() 메서드가 존재하기 때문에, 부모값을 탐색하러 가지 않음
         System.out.println("전기차를 빠르게 이동합니다.");
     }
 
