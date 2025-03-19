223d2283b4dbeaa7de69ded7aa0267e89a774eb4
daisy6365 <gksekqls9808@inu.ac.kr>
2025-03-19 17:41:42 +0900

    feat: 다형성과 캐스팅

--- src/poly/basic/CastingMain1.java
+++ src/poly/basic/CastingMain1.java
@@ -9,7 +9,14 @@ public class CastingMain1 {
         //poly.childMethod();
 
         //다운캐스팅(부모 타입 -> 자식 타입)
+        // 다운캐스팅을안하면 컴파일 오류가 발생
+        /**
+         * 1. 다운캐스팅을 통해 부모타입을 자식타입으로 변환
+         * 2. 참조 값을 읽은 다음 Child로 지정
+         * 3. poly가 자식타입이 된 것? X.
+         * 해당 참조값을 꺼내서 타입을 Child로 변경한 것
+         */
         Child child = (Child) poly; //x001
-        child.childMethod();
+        child.childMethod(); // Child 인스턴스의 기능을 호출 가능
     }
 }
